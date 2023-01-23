89e7c30378ccdb0668cf0b3e07b32afda3633b91
JakeGyberg <jakegyberg@gmail.com>
2023-01-22 17:52:23 -0800

    g

--- Board.java
+++ Board.java
@@ -1,3 +1,18 @@
 public class Board {
-    
+    private int pieces;
+
+    public Board()
+    {
+        pieces = (int) (Math.random() * 40) + 10;
+    }
+
+    public int getPieces()
+    {
+        return pieces;
+    }
+
+    public void removePieces (int num)
+    {
+        pieces -= num;
+    }
 }
--- Nim.java
+++ Nim.java
@@ -3,15 +3,48 @@ import java.util.Scanner;
 public class Nim {
     private Player player1;
     private Player player2;
+    private Board board;
+    private boolean playAgain;
+    private boolean playing;
+    private Player activePlayer;
 
     public Nim ()
     {
         player1 = new Player();
         player2 = new Player();
+        playAgain = false;
     }
 
     public void play()
     {
-        
+        Scanner input = new Scanner(System.in);
+
+        board = new Board();
+
+        //Select player to start
+        activePlayer = player1;
+        if (Math.random() > 0.5)
+        {
+            activePlayer = player2;
+        }
+
+        //Game loop
+        playing = true;
+        while(playing)
+        {
+            //prompt player for move
+            System.out.println(activePlayer.getName() + ", the pile has " + board.getPieces() + " pieces. How many will you remove?");
+            
+            board.removePieces(input.nextInt());
+            
+            //switch players
+            if (activePlayer.equals(player1))
+            {
+                activePlayer = player2;
+            } else
+            {  
+                activePlayer = player1;
+            }
+        }
     }
 }
